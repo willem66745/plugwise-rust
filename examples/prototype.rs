7fb034ffe6a54f4b4d35fca2e591f5e491bf82ea
Willem <willem66745@gmail.com>
2015-05-03 00:08:21 +0200

    Removed early implementation / hide low-level interfaces and users
    - merged examples to prototype.rs

--- examples/prototype.rs
+++ examples/prototype.rs
@@ -1,19 +1,37 @@
 
 extern crate plugwise;
+extern crate toml;
 
 use std::io;
+use std::io::prelude::*;
+use std::env::home_dir;
+use std::fs::File;
 
 use plugwise::ProtocolSnoop;
 
 fn main() {
-    let mut debug = io::stdout();
+    let mut path = home_dir().unwrap(); // XXX
+    path.push("plugwise.toml");
+    let mut file = File::open(&path).unwrap();
+    let mut config = String::new();
+    file.read_to_string(&mut config).unwrap();
+    let config = toml::Parser::new(&config).parse().unwrap(); // XXX
 
+    let mut debug = io::stdout();
     //let plugwise = plugwise::plugwise_device("/dev/ttyUSB0").unwrap();
     let plugwise = plugwise::plugwise_simulator().unwrap();
     plugwise.set_snoop(ProtocolSnoop::Debug(&mut debug));
-    let circle = plugwise.create_circle(0x0123456789ABCDEF).unwrap();
-    circle.switch_on().unwrap();
-    println!("{}", circle.is_switched_on().unwrap());
-    circle.switch_off().unwrap();
-    println!("{}", circle.is_switched_on().unwrap());
+
+    for (_, item) in config {
+        if let Some(mac) = item.as_table().unwrap().get("mac") { // XXX
+            let mac = mac.as_str().unwrap(); // XXX
+            let mac = u64::from_str_radix(mac, 16).unwrap(); // XXX
+
+            let circle = plugwise.create_circle(mac).unwrap();
+            circle.switch_on().unwrap();
+            println!("{}", circle.is_switched_on().unwrap());
+            circle.switch_off().unwrap();
+            println!("{}", circle.is_switched_on().unwrap());
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,8 @@ extern crate crc16;
 extern crate time;
 extern crate serial;
 
-pub mod sim; // XXX: remove
-pub mod real; // XXX: remove
-pub mod stub;
-pub mod protocol; // XXX: remove pub after abstractions are created
-
-// XXX: remove
-pub trait System {
-    fn register_plug(&mut self, id: &str) -> Box<Plug>;
-}
-
-// XXX: remove
-pub trait Plug {
-    fn get_id(&self) -> &str;
-    fn is_switched_on(&self) -> bool;
-    fn switch_on(&mut self);
-    fn switch_off(&mut self);
-}
-
+mod stub;
+mod protocol;
 
 use std::io::prelude::*;
 use std::io;
